f3f5affb379c214f4ab9897e3a3f83937e0bf22c
zenoamaro <zenoamaro@gmail.com>
2015-05-30 17:41:32 +0200

    Argument parsing and execution from command line.

--- qsh.py
+++ qsh.py
@@ -1,26 +1,47 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+import os
+import re
 import json
+import stat
+import argparse
+import tempfile
+import subprocess
 import urllib.request
 import urllib.parse
 
 GISTS_PER_PAGE = 100
 
+match_user_script_path = re.compile(r"""
+	(?P<username>[\w\d]+)
+	/
+	(?P<filename>[^/]+)
+""", re.VERBOSE)
 
-def fetch_user_script(user, filename):
+
+arg_parser = argparse.ArgumentParser(
+	description="Runs a script from a user's gists."
+)
+
+arg_parser.add_argument(
+	'script', help='A path to a script in the form `user/filename`.'
+)
+
+
+def fetch_user_script(username, filename):
 	"""
 	Searches for a gist containing the given file name.
 	"""
-	gists = fetch_user_gists(user)
+	gists = fetch_user_gists(username)
 	gistfile = find_file_in_gists(gists, filename)
-	if gistfile: return request('get', gistfile['raw_url'])
+	return request('get', gistfile['raw_url'])
 
 
-def fetch_user_gists(user, page=1):
+def fetch_user_gists(username, page=1):
 	"""
 	Returns a list of gists for a given user.
 	"""
-	url = 'https://api.github.com/users/{user}/gists'.format(user=user)
+	url = 'https://api.github.com/users/{user}/gists'.format(user=username)
 	params = { 'page': page, 'per_page': GISTS_PER_PAGE }
 	return request('get', url, params=params)
 
@@ -35,19 +56,25 @@ def find_file_in_gists(gists, target):
 				return contents
 
 
-def basename(filename):
-	"""
-	Returns a filename without the extension.
-	"""
-	return filename.rsplit('.', 1)[0]
+def parse_user_script_path(path):
+	match = match_user_script_path.match(path)
+	if not match: raise ValueError('Invalid script path, `{}`.'.format(path))
+	return match.groupdict()
 
 
 def filename_matches(a, b):
 	"""
 	Tries to matches two filenames exactly, then by basename.
 	"""
-	return a == b \
-	    or basename(a) == basename(b)
+	return (a == b) \
+	    or (basename(a) == basename(b))
+
+
+def basename(filename):
+	"""
+	Returns a filename without the extension.
+	"""
+	return filename.rsplit('.', 1)[0]
 
 
 def request(method, url, params=None, data=None):
@@ -59,9 +86,18 @@ def request(method, url, params=None, data=None):
 		method = method.upper(),
 	)
 	with urllib.request.urlopen(req) as f:
-		res = f.read().decode('utf-8')
+		res = f.read()
 		try:
-			return json.loads(res)
+			return json.loads(res.decode('utf-8'))
 		except ValueError:
 			return res
 			
+
+if __name__ == '__main__':
+	args = arg_parser.parse_args()
+	script = parse_user_script_path(args.script)
+	contents = fetch_user_script(script['username'], script['filename'])
+	with tempfile.NamedTemporaryFile(delete=True) as f:
+		f.write(contents) and f.flush()
+		os.chmod(f.name, stat.S_IRUSR|stat.S_IXUSR)
+		subprocess.call([ f.name ])
