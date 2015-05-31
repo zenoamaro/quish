c209c9713b6ff22ecf612b4f259762437f6c201f
zenoamaro <zenoamaro@gmail.com>
2015-05-31 15:02:57 +0200

    Much-needed improvements to the `request` util.

--- qsh.py
+++ qsh.py
@@ -41,7 +41,7 @@ def fetch_user_script(username, filename):
 	"""
 	gists = fetch_user_gists(username)
 	gistfile = find_file_in_gists(gists, filename)
-	return request('get', gistfile['raw_url'])
+	return request('get', gistfile['raw_url']).get('text')
 
 
 def fetch_user_gists(username, page=1):
@@ -50,7 +50,7 @@ def fetch_user_gists(username, page=1):
 	"""
 	url = 'https://api.github.com/users/{user}/gists'.format(user=username)
 	params = { 'page': page, 'per_page': GISTS_PER_PAGE }
-	return request('get', url, params=params)
+	return request('get', url, params=params).get('json')
 
 
 def find_file_in_gists(gists, target):
@@ -101,20 +101,38 @@ def basename(filename):
 	    or filename
 
 
-def request(method, url, params=None, data=None):
-	if params:
+def request(method, url, params=None, data=None, headers=None):
+	"""
+	Make a request to an URL, with optional query parameters
+	or body data.
+	"""
+	if params is not None:
 		params = urllib.parse.urlencode(params)
 		url = '{}?{}'.format(url, params)
-	req = urllib.request.Request(
-		url,
-		method = method.upper(),
+	if data is not None:
+		kwargs['data'] = json.dumps(data)
+	if headers is None:
+		headers = {}
+	headers['Content-Type'] = 'application/json;charset=utf-8'
+	request = urllib.request.Request(
+		url     = url,
+		headers = headers,
+		method  = method.upper(),
+		data    = data,
 	)
-	with urllib.request.urlopen(req) as f:
-		res = f.read()
+	with urllib.request.urlopen(request) as response:
+		text_content = response.read().decode('utf-8')
 		try:
-			return json.loads(res.decode('utf-8'))
+			json_content = json.loads(text_content)
 		except ValueError:
-			return res
+			json_content = None
+		return {
+			'status': response.status,
+			'reason': response.reason,
+			'headers': response.getheaders(),
+			'text': text_content,
+			'json': json_content,
+		}
 			
 
 if __name__ == '__main__':
@@ -125,6 +143,6 @@ if __name__ == '__main__':
 	# Store the contents to a named temporary file and make it
 	# executable so that it is callable with subprocess.
 	with tempfile.NamedTemporaryFile(delete=True) as f:
-		f.write(contents) and f.flush()
+		f.write(contents.encode('utf-8')) and f.flush()
 		os.chmod(f.name, stat.S_IRUSR|stat.S_IXUSR)
 		subprocess.call([ f.name ] + arguments, stdin=sys.stdin)
